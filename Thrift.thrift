2ca9a2dcef7426189911e2f3c4bc500ea5fd9763
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-09-01 18:11:04 +0900

    Update Thrift.thrift

--- Thrift.thrift
+++ Thrift.thrift
@@ -1,3 +1,18 @@
+namespace java tutorial
+namespace py tutorial
+
+/*
+ C like comments are supported
+*/
+// This is also a valid comment
+
+typedef i32 int // We can use typedef to get pretty names for the types we are using
+service MultiplicationService
+{
+        int multiply(1:int n1, 2:int n2),
+}
+
+
 enum PhoneType {
   HOME,
   WORK,
@@ -15,3 +30,71 @@ service PhoneService {
   Phone findById(1: i32 id),
   list<Phone> findAll()
 }
+
+thrift --gen java multiplication.thrift
+thrift --gen py multiplication.thrift
+thrift -r --gen java filame.thrift
+
+
+include "shared.thrift"
+
+enum Operation {
+  ADD = 1,
+  SUBTRACT = 2,
+  MULTIPLY = 3,
+  DIVIDE = 4
+}
+
+struct Work {
+  1: i32 num1 = 0,
+  2: i32 num2,
+  3: Operation op,
+  4: optional string comment,
+}
+
+exception InvalidOperation {
+  1: i32 what,
+  2: string why
+}
+
+service Calculator extends shared.SharedService {
+
+   void ping(),
+
+   i32 add(1:i32 num1, 2:i32 num2),
+
+   i32 calculate(1:i32 logid, 2:Work w) throws (1:InvalidOperation ouch),
+
+   /**
+    * This method has a oneway modifier. That means the client only makes
+    * a request and does not listen for any response at all. Oneway methods
+    * must be void.
+    */
+   oneway void zip()
+
+}
+
+thrift [options] file
+
+namespace java tutorial
+namespace py tutorial
+
+typedef i32 int // We can use typedef to get pretty names for the types we are using
+service MultiplicationService
+{
+        int multiply(1:int n1, 2:int n2),
+}
+
+import org.apache.thrift.TException;
+
+public class MultiplicationHandler implements MultiplicationService.Iface {
+
+	@Override
+	 public int multiply(int n1, int n2) throws TException {
+	    System.out.println("Multiply(" + n1 + "," + n2 + ")");
+	    return n1 * n2;
+	 }
+
+	
+}
+
